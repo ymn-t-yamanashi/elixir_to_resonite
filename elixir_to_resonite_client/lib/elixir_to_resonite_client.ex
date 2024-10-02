38b135aed11ebc28f4cb16f473cfb3904b0e988a
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-02 23:53:34 +0900

    リファクタリング

--- elixir_to_resonite_client/lib/elixir_to_resonite_client.ex
+++ elixir_to_resonite_client/lib/elixir_to_resonite_client.ex
@@ -17,20 +17,25 @@ defmodule ElixirToResoniteClient do
 
     F.init_frame()
     |> F.copy("Box", "TestBo2")
-    |> F.create_instructions()
-    |> Socket.send_instructions(socket, 10)
+    |> Socket.send_frame(socket, 0)
 
     -10..200
-    |> Enum.map(fn x ->
-      F.init_frame()
-      |> F.move("Cylinder", x * 0.1, x * 0.1, 2.0)
-      |> F.move("Box", 1.0, 2.0, x * 0.1)
-    end)
+    |> create_instructions()
     |> Socket.send_instructions(socket, 10)
 
     F.init_frame()
     |> F.delete("TestBo2")
-    |> F.create_instructions()
-    |> Socket.send_instructions(socket, 10)
+    |> Socket.send_frame(socket, 0)
+  end
+
+  def create_instructions(instruction) do
+    instruction
+    |> Enum.map(&create_frame(&1))
+  end
+
+  defp create_frame(x) do
+    F.init_frame()
+    |> F.move("Cylinder", x * 0.1, x * 0.1, 2.0)
+    |> F.move("Box", 1.0, 2.0, x * 0.1)
   end
 end
