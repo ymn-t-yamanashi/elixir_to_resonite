00a7e900af2c4c31b17291c887a9237403b9b693
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-04 18:51:40 +0900

    midi_in作成

--- midi_to_resonite/lib/midi_to_resonite.ex
+++ midi_to_resonite/lib/midi_to_resonite.ex
@@ -19,6 +19,20 @@ defmodule MidiToResonite do
     |> F.move("Box", 1, 1, 2)
     |> Socket.send_frame(socket, 10)
 
+    {:ok, input} = PortMidi.open(:input, "Midi Through Port-0")
+    PortMidi.listen(input, self())
+    midi_in(input)
+
+    PortMidi.close(:output, input)
+
     :world
+  end
+
+  def midi_in(input) do
+    receive do
+      data ->
+        IO.inspect(data)
+        midi_in(input)
+    end
   end
 end
