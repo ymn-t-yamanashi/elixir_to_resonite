9ba8e0673bea03b226523888cbc426396f52f6eb
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-04 19:20:56 +0900

    get_midi_data作成

--- midi_to_resonite/lib/midi_to_resonite.ex
+++ midi_to_resonite/lib/midi_to_resonite.ex
@@ -21,31 +21,40 @@ defmodule MidiToResonite do
 
     {:ok, input} = PortMidi.open(:input, "Midi Through Port-0")
     PortMidi.listen(input, self())
-    midi_in(input)
+    midi_in(input, socket)
 
     PortMidi.close(:output, input)
 
     :world
   end
 
-  def midi_in(input) do
+  def midi_in(input, socket) do
     receive do
       data ->
-        get_data(data)
-        midi_in(input)
+        get_data(data, socket)
+        midi_in(input, socket)
     end
   end
 
-  def get_data({_, []}), do: nil
+  def get_data({_, []}, _), do: nil
 
-  def get_data({_, data}) do
-    IO.inspect("--------------")
-
+  def get_data({_, data}, socket) do
     data
-    |> Enum.each(fn x -> get_midi_data(x) end)
+    |> Enum.each(fn x -> get_midi_data(x, socket) end)
   end
 
-  def get_midi_data({data, _}) do
-    IO.inspect(data)
+  def get_midi_data({{_, x, _}, _}, socket) do
+    v =
+      case x do
+        48 -> 1
+        53 -> 2
+        60 -> 3
+      end
+
+    F.init_frame()
+    |> F.move("Box", 1, v, 1)
+    |> Socket.send_frame(socket, 0)
+
+    IO.inspect(v)
   end
 end
