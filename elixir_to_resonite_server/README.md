6f7097cb72c4b2546ba10883e3c54fb1a0d0e0a8
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-12 10:59:26 +0900

    mix phx.gen.release --docker

--- elixir_to_resonite_server/README.md
+++ elixir_to_resonite_server/README.md
@@ -1,18 +1,2 @@
-# ElixirToResoniteServer
-
-To start your Phoenix server:
-
-  * Run `mix setup` to install and setup dependencies
-  * Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
-
-Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
-
-Ready to run in production? Please [check our deployment guides](https://hexdocs.pm/phoenix/deployment.html).
-
-## Learn more
-
-  * Official website: https://www.phoenixframework.org/
-  * Guides: https://hexdocs.pm/phoenix/overview.html
-  * Docs: https://hexdocs.pm/phoenix
-  * Forum: https://elixirforum.com/c/phoenix-forum
-  * Source: https://github.com/phoenixframework/phoenix
+docker build -t rsv .
+docker run rsv -p 4000:4000
--- elixir_to_resonite_server/config/runtime.exs
+++ elixir_to_resonite_server/config/runtime.exs
@@ -26,20 +26,20 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  # secret_key_base =
+  #   System.get_env("SECRET_KEY_BASE") ||
+  #     raise """
+  #     environment variable SECRET_KEY_BASE is missing.
+  #     You can generate one by calling: mix phx.gen.secret
+  #     """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  # host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :elixir_to_resonite_server, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :elixir_to_resonite_server, ElixirToResoniteServerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    # url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
@@ -48,7 +48,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: "t95kbT192Z8gKjzgeoxCp8snU0YdlpJPx5tA2oUwma49terfHoyvT7IWvquYHSTv"
+    #secret_key_base: secret_key_base
 
   # ## SSL Support
   #
